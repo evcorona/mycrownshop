be67718cb024f88526fdee9c1c2190a63b579d2d
veroxcrown <eldav.corona@gmail.com>
2020-12-09 18:26:17 -0600

    uploading layout

---
+++ js/main.js
@@ -0,0 +1,13 @@
+//https://ajaxclass-1ca34.firebaseio.com/orders/.json
+//https://ajaxclass-1ca34.firebaseio.com/items/.json
+/*formulario para cargar más productos,
+  category debe ser alguna de las siguientes opciones:
+  category:"fruit","vegetable", "meat", "miscelaneous"
+  unit:"kg", "lt", "pz"*/
+/*crear vista para mostrar todos los productos*/
+/*permitir filtrar productos por categoría*/
+/*cada producto debe tener un botón de "agregar al carrito", debe pedir la cantidad de cada producto*/
+/*crear vista de carrito de compras*/
+/*debe tener la lista de productos en el carrito, mostrando el nombre, la cantidad y el precio*/
+/*debe mostrar el costo total de la compra*/
+
